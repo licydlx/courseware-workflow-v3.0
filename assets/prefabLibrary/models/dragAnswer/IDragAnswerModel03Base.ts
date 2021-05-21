b6b4ccafd7e46155c4d620e78f30ca851beb1665
guhl <guhongliang@37.com>
2021-05-21 19:36:06 +0800

    remove

--- assets/prefabLibrary/models/dragAnswer/IDragAnswerModel03Base.ts
+++
@@ -1,5 +0,0 @@
-export default interface IDragAnswerModel03Base {
-    updateUi(),
-    mergeState(),
-    onLoad(),
-}
--- library/imports/17/1736cc9c-6bd6-4736-83a8-58fbad533cda.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-cc._RF.push(module, '1736cyca9ZHNoOoWPutUzza', 'IDragAnswerModel03Base');
-// prefabLibrary/models/dragAnswer/IDragAnswerModel03Base.ts
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-cc._RF.pop();
